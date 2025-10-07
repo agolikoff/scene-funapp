2a11782e0bce867cb09223610b7839dd4c23421b
Anton <4natic@gmail.com>
2025-10-07 22:12:26 +0300

    fix

--- proxy.js
+++ proxy.js
@@ -1,11 +1,8 @@
-import 'dotenv/config';
+require('dotenv').config();
 import express from 'express';
 import axios from 'axios';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
